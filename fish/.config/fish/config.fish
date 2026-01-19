4fe505e198020d434d24aca8a7faa171786e96e9
Levy Gu <32436334@qq.com>
2026-01-19 19:26:00 +0800

    fcitx5

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,7 +7,7 @@ starship init fish | source
 alias rebuild="sudo nixos-rebuild switch"
 alias vimnix="hx ~/.dotfiles/nixos/configuration.nix"
 alias nshell="nix-shell -p"
-alias stowdot="stow -d ~/.dotfiles/ fish vim niri starship waybar helix jj  && source ~/.config/fish/config.fish"
+alias stowdot="stow -d ~/.dotfiles/ fish vim niri starship waybar helix jj fcitx5 rime  && source ~/.config/fish/config.fish"
 alias stnix="sudo stow -d ~/.dotfiles/ nixos -t /etc/nixos"
 alias yz="yazi"
 
