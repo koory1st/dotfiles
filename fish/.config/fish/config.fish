b0183ce3f48fe87ad200331209712a2eabe5f3ab
Levy Gu <32436334@qq.com>
2026-04-08 20:01:01 +0800

    fix: add sesh

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,7 +10,7 @@ end
 alias rebuild="sudo nixos-rebuild switch"
 alias vimnix="hx ~/.dotfiles/nixos/configuration.nix"
 alias nshell="nix-shell -p"
-alias stowdot="stow -d ~/.dotfiles/ fish vim niri starship waybar helix jj fcitx5 cargo opencode ghostty television --adopt && source ~/.config/fish/config.fish"
+alias stowdot="stow -d ~/.dotfiles/ fish vim niri starship waybar helix jj fcitx5 cargo opencode ghostty television tmux --adopt && source ~/.config/fish/config.fish"
 alias stnix="sudo stow -d ~/.dotfiles/ nixos -t /etc/nixos"
 alias yz="yazi"
 alias hx="helix"
@@ -18,6 +18,10 @@ alias hx="helix"
 # tv shell history
 if command -v tv >/dev/null 2>&1
     tv init fish | source
+end
+
+if command -v sesh >/dev/null 2>&1; and not test -f ~/.config/fish/completions/sesh.fish
+    sesh completion fish >~/.config/fish/completions/sesh.fish
 end
 
 # atuin shell history
