fa34c479acb9bdcd1cbe8ec86a39c9314acfaf93
Levy Gu <32436334@qq.com>
2026-04-09 19:30:07 +0800

    fix: fish proxy

--- fish/.config/fish/functions/proxy-on.fish
+++ fish/.config/fish/functions/proxy-on.fish
@@ -1,11 +1,11 @@
 # 定义开启 HTTP/HTTPS 代理的函数
 function proxy-on
-    export http_proxy http://127.0.0.1:7897
-    export https_proxy http://127.0.0.1:7897
-    export all_proxy socks5://127.0.0.1:7897
+    set -gx http_proxy "http://127.0.0.1:7897"
+    set -gx https_proxy "http://127.0.0.1:7897"
+    set -gx all_proxy "socks5://127.0.0.1:7897"
 
-    git config --global http.proxy http://127.0.0.1:7897
-    git config --global https.proxy http://127.0.0.1:7897
+    git config --global http.proxy "http://127.0.0.1:7897"
+    git config --global https.proxy "http://127.0.0.1:7897"
 
     echo "HTTP/HTTPS 代理已开启："
     echo "http_proxy: $http_proxy"
