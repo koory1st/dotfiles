2ae710d58d0c3117e7016ae99b24ffbcfe1e68d1
Levy Gu <32436334@qq.com>
2026-04-09 19:30:07 +0800

    fix: fish proxy

--- fish/.config/fish/functions/proxy-off.fish
+++ fish/.config/fish/functions/proxy-off.fish
@@ -1,15 +1,11 @@
 # 定义关闭 HTTP/HTTPS 代理的函数
 function proxy-off
-    # 清除所有代理相关环境变量
     set -e http_proxy
     set -e https_proxy
     set -e all_proxy
-    set -e HTTP_PROXY
-    set -e HTTPS_PROXY
-    set -e ALL_PROXY
 
     git config --global --unset http.proxy
     git config --global --unset https.proxy
-    # 提示代理已关闭
+
     echo "HTTP/HTTPS 代理已关闭"
 end
